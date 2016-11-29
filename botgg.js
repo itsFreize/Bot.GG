36745cb991b1d178d1c4927e59c692e576b9d481
itsFreize <itsfreize@gmail.com>
2016-11-29 18:31:32 +0000

    Updating for openshift

--- botgg.js
+++ botgg.js
@@ -288,5 +288,12 @@ bot.on('message', message =>{
   //   });
   // }
 });
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 5858
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function() {
+  console.log("Listening on " + server_ip_address + ", port " + server_port)
+});
 //---This logs the bot in
 bot.login(config.token)
